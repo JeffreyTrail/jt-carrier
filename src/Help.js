4ff0e7a063d9d3cc3bf371b2a26f28ac7122d8ec
Rango <34990962+the-rango@users.noreply.github.com>
2022-08-11 20:26:32 -0700

    Update Drawing Rules & FAQ

--- src/Help.js
+++ src/Help.js
@@ -32,7 +32,7 @@ function Help() {
             <ol>
             <li>Each week, there is a drawing for each homeroom. The prize is distributed by ASB each week in homeroom.</li>
             <li>Each week, we hold a drawing for each of the WINGS letters for all tickets submitted in the whole school. Pay close attention during morning announcements. </li>
-            <li>This last one is not a drawing. Some WINGS tickets are just special - they're golden tickets, which will reward you immediately after you redeem them.</li>
+            {/*<li>This last one is not a drawing. Some WINGS tickets are just special - they're golden tickets, which will reward you immediately after you redeem them.</li>*/}
             </ol>
           </Typography>
         </AccordionDetails>
@@ -47,7 +47,7 @@ function Help() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            A: Each week starts at 0:00 on Monday and ends by midnight of the following Sunday.
+            A: Each week starts at 0:00 on Tuesday and ends by midnight of the following Monday.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -62,7 +62,7 @@ function Help() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            A: Drawings are conducted every Tuesday! Pay attention to your morning announcements!
+            A: Homeroom drawings are conducted every Tuesday! Your homeroom reps will come to homeroom with the prize and announce the winner. Letter drawing winners will find out on Fridays in the morning announcements.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -77,7 +77,7 @@ function Help() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            A: Homeroom drawing prizes change every week; it's listed under the Stats tab. Letter drawing winners get to choose one of many gift cards. Golden ticket winners will have a variety of surprises too.
+            A: Homeroom drawing prizes change every week; it's listed under the Stats tab. Letter drawing winners get to choose one of many gift cards.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -93,6 +93,21 @@ function Help() {
         <AccordionDetails>
           <Typography>
             A: Hold onto them! For one thing, they are a memory of your time at JTMS; for another, they might come in handy later on this year...
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="q3-content"
+          id="q3-header"
+        >
+          <Typography>6. Where can I find more information?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            A: Read more in <a href="https://docs.google.com/presentation/d/1H-LzoqsSk9vMl3m1fxmCqVXjY4k1E_7nsb68xoCAiqU/edit?usp=sharing" target="_blank" rel="noopener">this slides deck here</a>. If you have any further questions, please submit them to <a href="https://forms.gle/t9R29o6ZJXDM7NE37" target="_blank" rel="noopener">the feedback form</a>!
           </Typography>
         </AccordionDetails>
       </Accordion>
