0810788f8625f408aa52af6f1ddfe5da0ebb8f5f
Rango <34990962+the-rango@users.noreply.github.com>
2022-10-13 09:10:39 -0700

    url field for each event

--- src/Events.js
+++ src/Events.js
@@ -16,51 +16,43 @@ const ads = [
   {
     img: 'https://variety.com/wp-content/uploads/2022/06/Is-It-Cake.jpg?w=681&h=383&crop=1',
     title: "Is It Cake Judging",
+    url: "https://bit.ly/2022IsItCake",
+  },
+  {
+    img: 'https://jeffreytrail.iusd.org/sites/jeffreytrail/files/files/Schools/JT/bell_schedule_22.23.jpg',
+    title: "Bell Schedule",
+    url: "https://jeffreytrail.iusd.org/sites/jeffreytrail/files/files/Schools/JT/bell_schedule_22.23.jpg",
   },
   {
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJkAk-5OjH6ntOXNLGE_o6YoLtC7ShVMq2McwaHCkVc8hNFdSZ6BmvoxtHe8jm3HUC0x8&usqp=CAU',
     title: 'Random Children on Field Next to Georgino',
+    url: "https://jeffreytrail.iusd.org/",
   },
   {
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIi8pdfmCe-N4C_mC_jHUTUY5IJ_1V80IA4Q&usqp=CAU',
     title: 'Cool Thing with Mr. Nguyen',
+    url: "https://jtmsrobotics.wixsite.com/jtms",
   },
   {
     img: 'https://m.media-amazon.com/images/S/aplus-media/sc/d469aa8e-bcf0-4253-b71e-dd56a2d93124.__CR0,0,300,300_PT0_SX300_V1___.jpg',
     title: 'Oranges on Vines',
-  },
-  {
-    img: 'https://m.media-amazon.com/images/S/aplus-media/sc/805a00ac-194f-4c6d-8b6f-7dad967f39fd.__CR0,0,300,300_PT0_SX300_V1___.jpg',
-    title: 'Oranges on Table',
+    url: "https://jeffreytrail.iusd.org/",
   },
   {
     img: 'https://m.media-amazon.com/images/S/aplus-media/sc/dacc10c3-706c-40cf-a948-a7de629274a1.__CR0,0,300,300_PT0_SX300_V1___.jpg',
     title: 'Happy People around Oranges',
+    url: "https://jeffreytrail.iusd.org/",
   },
   {
     img: 'https://m.media-amazon.com/images/S/aplus-media-library-service-media/03d50b82-7bbf-421d-8bb7-a8f1f63e8be6.__CR0,0,220,220_PT0_SX220_V1___.jpg',
     title: 'Orange for Dinner',
-  },
-  {
-    img: 'https://m.media-amazon.com/images/S/aplus-media-library-service-media/cd99bff7-4921-410f-b296-dfb7af85d993.__CR0,0,970,300_PT0_SX970_V1___.jpg',
-    title: 'Aesthetic Oranges',
+    url: "https://jeffreytrail.iusd.org/",
   },
   {
     img: 'https://m.media-amazon.com/images/S/aplus-media-library-service-media/c7324650-14ee-411b-ae49-8d69b97935bb.__CR0,0,970,600_PT0_SX970_V1___.jpg',
     title: 'Assorted Oranges',
+    url: "https://jeffreytrail.iusd.org/",
   },
-  {
-    img: 'https://drive.google.com/uc?id=17BHh0lKx6LMFhI9mMtfGuG9sj7OO-_nk',
-    title: 'tired cat',
-  },
-  // {
-  //   img: 'https://drive.google.com/uc?id=1uaGJ2B2xAFabETa1vi8PRzuad02kXr8T',
-  //   title: 'cat',
-  // },
-  // {
-  //   img: 'https://drive.google.com/uc?id=14zIK4PUtukFzWm-fTASV-dyWCssMxk5v',
-  //   title: 'confused cat',
-  // },
 ];
 
 function Events() {
@@ -99,7 +91,7 @@ function Events() {
                 <IconButton
                   sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
                   aria-label={`info about ${item.title}`}
-                  href="https://bit.ly/2022IsItCake"
+                  href={item.url}
                   target="_blank"
                   rel="noreferrer"
                 >
