bee988b3b32ca39177838973d014daf853ff685f
Rango <34990962+the-rango@users.noreply.github.com>
2022-10-15 19:34:38 -0700

    Added Ads with Info Field

--- src/Adv.js
+++ src/Adv.js
@@ -12,8 +12,8 @@ function Adv() {
     }}>
       <Typography variant="h5" sx={rowStyle}>Advantage Flight Plan</Typography>
       <br />
-      <Typography variant="body1">Make an appointment at <a href="https://teachmore.org/jeffreytrail/students/" target="_blank" rel="noreferrer">https://teachmore.org/jeffreytrail/students/</a></Typography>
-      <Typography variant="body1">Make sure to make an appointment for both Wednesday and Thursday and to check your teacher's advantage offering below.</Typography>
+      <Typography variant="h6">Make an appointment at <a href="https://teachmore.org/jeffreytrail/students/" target="_blank" rel="noreferrer">https://teachmore.org/jeffreytrail/students/</a></Typography>
+      <Typography variant="h6">Make sure to make an appointment for both Wednesday and Thursday and to check your teacher's advantage offering below.</Typography>
       <br />
       <iframe title="Advantage Itinerary" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSwlxnBZge1D9VDll4722dUVfLXyWX-WeY7B0PbQ8J7YSs0Fdh_llOTq3IzC_F51yy1lx6MutcxokPI/pubhtml?widget=true&amp;headers=false" width="100%" height="1500">
       </iframe>
--- src/Events.js
+++ src/Events.js
@@ -4,7 +4,6 @@ import {
   ImageList,
   ImageListItem,
   ImageListItemBar,
-  Chip,
   IconButton,
 } from "@mui/material";
 import {
@@ -14,44 +13,40 @@ import * as React from "react";
 
 const ads = [
   {
-    img: 'https://variety.com/wp-content/uploads/2022/06/Is-It-Cake.jpg?w=681&h=383&crop=1',
-    title: "Is It Cake Judging",
-    url: "https://bit.ly/2022IsItCake",
+    img: 'https://www.stancoe.org/sites/default/files/styles/banner/public/banners/college%20ready.jpg?itok=MEVz3B7A',
+    title: "College & Career Fair",
+    info: "Sign up for IUSD's CCF at Woodbridge HS 11/07 5-8pm",
+    url: "https://drive.google.com/file/d/1ULa1SohPLqEhmVVNbEi83s_mWQGZgFnX/view?usp=sharing",
   },
   {
-    img: 'https://jeffreytrail.iusd.org/sites/jeffreytrail/files/files/Schools/JT/bell_schedule_22.23.jpg',
+    img: 'https://media.istockphoto.com/photos/white-wall-clock-on-blue-background-picture-id1289661784?b=1&k=20&m=1289661784&s=170667a&w=0&h=pNZcN6beC8ov_ZZGwFiFxqdFLR8GQUomjBpnz8vI_8k=',
     title: "Bell Schedule",
-    url: "https://jeffreytrail.iusd.org/sites/jeffreytrail/files/files/Schools/JT/bell_schedule_22.23.jpg",
+    info: "Click here >>>",
+    url: "https://jeffreytrail.iusd.org/about/calendar-events/bell-schedule",
   },
   {
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJkAk-5OjH6ntOXNLGE_o6YoLtC7ShVMq2McwaHCkVc8hNFdSZ6BmvoxtHe8jm3HUC0x8&usqp=CAU',
-    title: 'Random Children on Field Next to Georgino',
-    url: "https://jeffreytrail.iusd.org/",
+    img: 'https://drive.google.com/uc?id=16a3BXjQDtgg8uv2YjOka8kE6UDLrKrk0',
+    title: "Rubik's Cube Club",
+    info: "We turn colorful cubes quickly",
+    url: "http://bit.ly/JTMSCubingClub",
   },
   {
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIi8pdfmCe-N4C_mC_jHUTUY5IJ_1V80IA4Q&usqp=CAU',
-    title: 'Cool Thing with Mr. Nguyen',
-    url: "https://jtmsrobotics.wixsite.com/jtms",
+    img: 'https://drive.google.com/uc?id=1UResVzqzEWMyKx6Xysc3hIRJ0hQ7E1iV',
+    title: 'WE Team',
+    info: "WE Team is dedicated to helping people in need of support.",
+    url: "http://bit.ly/weteamweb",
   },
   {
-    img: 'https://m.media-amazon.com/images/S/aplus-media/sc/d469aa8e-bcf0-4253-b71e-dd56a2d93124.__CR0,0,300,300_PT0_SX300_V1___.jpg',
-    title: 'Oranges on Vines',
-    url: "https://jeffreytrail.iusd.org/",
+    img: 'https://drive.google.com/uc?id=14vIn0blNYbOY-M5yiYmAZ2YcGpczEFk8',
+    title: 'Editing Club',
+    info: "Editing club is where you can learn to edit and have fun.",
+    url: "",
   },
   {
-    img: 'https://m.media-amazon.com/images/S/aplus-media/sc/dacc10c3-706c-40cf-a948-a7de629274a1.__CR0,0,300,300_PT0_SX300_V1___.jpg',
-    title: 'Happy People around Oranges',
-    url: "https://jeffreytrail.iusd.org/",
-  },
-  {
-    img: 'https://m.media-amazon.com/images/S/aplus-media-library-service-media/03d50b82-7bbf-421d-8bb7-a8f1f63e8be6.__CR0,0,220,220_PT0_SX220_V1___.jpg',
-    title: 'Orange for Dinner',
-    url: "https://jeffreytrail.iusd.org/",
-  },
-  {
-    img: 'https://m.media-amazon.com/images/S/aplus-media-library-service-media/c7324650-14ee-411b-ae49-8d69b97935bb.__CR0,0,970,600_PT0_SX970_V1___.jpg',
-    title: 'Assorted Oranges',
-    url: "https://jeffreytrail.iusd.org/",
+    img: 'https://drive.google.com/uc?id=1G_4Oejf47D8UoXtch54ytl-P4gME_4Jw',
+    title: 'Jet Gazette (Student News)',
+    info: "Jet Gazette Student Newspaper",
+    url: "",
   },
 ];
 
@@ -64,7 +59,6 @@ function Events() {
     }}>
       <Box sx={rowStyle}>
         <Typography variant="h5">What's Happening at JTMS?</Typography>
-        <Chip label="COMING SOON" color="primary" variant="outlined" size="small" sx={{marginLeft: 2}}/>
       </Box>
 
       <br />
@@ -86,8 +80,8 @@ function Events() {
             />
             <ImageListItemBar
               title={item.title}
-              subtitle={"This is a placeholder. Legit events coming soon."}
-              actionIcon={
+              subtitle={item.info}
+              actionIcon={item.url === "" ? "" :
                 <IconButton
                   sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
                   aria-label={`info about ${item.title}`}
