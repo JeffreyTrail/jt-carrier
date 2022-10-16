c8c7f119c3c27a538f7ce7c7df8dbc5a7f1bcbb4
Rango <34990962+the-rango@users.noreply.github.com>
2022-10-15 19:53:59 -0700

    Added Special Sched

--- src/Events.js
+++ src/Events.js
@@ -20,6 +20,12 @@ const ads = [
   },
   {
     img: 'https://media.istockphoto.com/photos/white-wall-clock-on-blue-background-picture-id1289661784?b=1&k=20&m=1289661784&s=170667a&w=0&h=pNZcN6beC8ov_ZZGwFiFxqdFLR8GQUomjBpnz8vI_8k=',
+    title: "Special Min Day Schedule",
+    info: "Conference Week: Tue 10/18 & Fri 10/20",
+    url: "https://docs.google.com/document/d/1X4CVrXRRRGvaHjspfkt2wWqzgi4HrBc_On3TaBquf6A/edit?usp=sharing",
+  },
+  {
+    img: 'https://media.istockphoto.com/photos/white-wall-clock-on-blue-background-picture-id1289661784?b=1&k=20&m=1289661784&s=170667a&w=0&h=pNZcN6beC8ov_ZZGwFiFxqdFLR8GQUomjBpnz8vI_8k=',
     title: "Bell Schedule",
     info: "Click here >>>",
     url: "https://jeffreytrail.iusd.org/about/calendar-events/bell-schedule",
